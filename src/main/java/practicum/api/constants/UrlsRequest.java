6208f1b848a50323a4a89d2fc7cf720d8616193c
Boris Rogov <boriakin97@gmail.com>
2024-09-12 13:57:00 +0300

    Add file with urls

---
+++ src/main/java/practicum/api/constants/UrlsRequest.java
@@ -0,0 +1,13 @@
+package practicum.api.constants;
+
+public class UrlsRequest {
+
+    public static final String BASE_URL = "http://qa-scooter.praktikum-services.ru";
+    public static final String COURIER_URL = "api/v1/courier";
+    public static final String LOGIN_COURIER_URL = "api/v1/courier/login";
+    public static final String DELETE_COURIER_URL = COURIER_URL + "/{id}";
+    public static final String ORDERS_URL = "/api/v1/orders";
+    public static final String CANCEL_ORDER_URL = "/api/v1/orders/cancel";
+
+
+}
